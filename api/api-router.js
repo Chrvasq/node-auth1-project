88929026265c90467aec4875eb44208437f1185f
Chris Vasquez <chrvasq@outlook.com>
2020-02-17 22:23:36 -0800

    Update session to include user ID

--- api/api-router.js
+++ api/api-router.js
@@ -32,7 +32,7 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        req.session.loggedIn = true;
+        req.session.userId = user.id;
         res.status(200).json({ message: `Welcome ${user.username}.` });
       } else {
         res.status(401).json({ message: "Invalid credentials." });
@@ -46,7 +46,7 @@ router.post("/login", (req, res) => {
 
 // Logout user
 router.delete("/logout", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.userId) {
     req.session.destroy(err => {
       // check if err exists
       err
