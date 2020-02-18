1815a8ab73d546ec61ba667bebac429039b34f4a
Chris Vasquez <chrvasq@outlook.com>
2020-02-17 22:09:26 -0800

    Refactor auth-required-middleware to check for session

--- auth/auth-required-middleware.js
+++ auth/auth-required-middleware.js
@@ -2,23 +2,9 @@ const Users = require("../users/users-model");
 const bcrypt = require("bcryptjs");
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
-
-  if (!(username && password)) {
-    res.status(401).json({ message: "Missing credentials." });
+  if (req.session.loggedIn === true) {
+    next();
   } else {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You shall not pass!" });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json({ message: err });
-      });
+    res.status(400).json({ message: "You shall not pass!" });
   }
 };
