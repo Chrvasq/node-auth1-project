7fdfca3c6198acdc50aabddf4f9819e8abc70bc0
Chris Vasquez <chrvasq@outlook.com>
2020-02-17 22:09:51 -0800

    Refactor auth-router

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,14 +1,6 @@
 const router = require("express").Router();
 const Users = require("../users/users-model");
 
-// POST login user
-router.post("/login", (req, res) => {
-  // add login logic here
-  const { username } = req.headers;
-
-  res.status(200).json({ message: `Welcome ${username}.` });
-});
-
 // GET users
 router.get("/users", (req, res) => {
   Users.find()
