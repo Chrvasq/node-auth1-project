d3059a4feb1dcd6a63ee132d51015a3267bb88c4
Chris Vasquez <chrvasq@outlook.com>
2020-02-17 22:08:24 -0800

    Add express-session and connect-session-knex middleware and refactor login endpoint and add logout endpoint

--- api/api-router.js
+++ api/api-router.js
@@ -10,10 +10,10 @@ router.get("/", (req, res) => {
   res.json({ api: "It's alive!" });
 });
 
-// POST register new user
+// Register new user
 router.post("/register", (req, res) => {
   const user = req.body;
-  const hash = bcrypt.hashSync(user.password, 14);
+  const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
 
   Users.add(user)
@@ -22,6 +22,40 @@ router.post("/register", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+// Login user
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedIn = true;
+        res.status(200).json({ message: `Welcome ${user.username}.` });
+      } else {
+        res.status(401).json({ message: "Invalid credentials." });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ errorMessage: err });
+    });
+});
+
+// Logout user
+router.delete("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(err => {
+      // check if err exists
+      err
+        ? res.status(400).send("Can't logout")
+        : res.send("You've successfully logged out.");
+    });
+  } else {
+    res.end();
+  }
 });
 
 module.exports = router;
